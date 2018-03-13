f1ea940c044a8e266646d5d56b9a504b8fcc0c50
Brandon Dube <brandondube@gmail.com>
2018-03-13 01:35:52 -0400

    finally nice yams

--- wt/core.py
+++ wt/core.py
@@ -7,7 +7,8 @@ from functools import reduce, partial
 from ruamel.yaml import YAML
 import pandas as pd
 
-yaml = YAML()
+yaml = YAML(typ='unsafe')
+yaml.default_flow_style = False
 yaml.indent(sequence=4, mapping=2, offset=2)
 
 
